2c141b62b0ca31b98141ff1cf1f0ee41b4f8653e
phanh <anhtp@nftech.io>
2026-03-12 15:50:02 +0700

    update collection api

--- src/routes/collections.js
+++ src/routes/collections.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getCollections } from "../controllers/collectionController.js";
+import { getCollections, getCollectionBySlug } from "../controllers/collectionController.js";
 
 const router = express.Router();
 
 router.get("/", getCollections);
+router.get("/:slug", getCollectionBySlug);
 
 export default router;
